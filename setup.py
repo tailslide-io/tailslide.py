c1c30d1e7b1a436493b12a555608aa8c77ac0bad
Steven Liou <stevenliou@gmail.com>
2022-07-13 18:11:05 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='tailslide-sdk',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Steven Liou, Trent Cooper, Elaine Vuong, Jordan Eggers',
+    author_email='tailslide.io.2205@gmail.com ',
+    license='MIT',
+    packages=['src/tailslide'],
+    install_requires=[
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
