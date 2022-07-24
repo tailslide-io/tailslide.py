c0040c6fcf83638eb11a0e35a6bb66f3b4c84b5a
Steven Liou <stevenliou@gmail.com>
2022-07-24 15:30:01 -0400

    Refactors RedisTimeSeriesClient

--- src/tailslide/redis_timeseries_client.py
+++ src/tailslide/redis_timeseries_client.py
@@ -2,13 +2,13 @@ import redis
 
 
 class RedisTimeSeriesClient:
-  def __init__(self, redis_host, redis_port):
-    self.redis_host = redis_host or 'localhost'
-    self.redis_port = redis_port or 6379
+  def __init__(self, host, port):
+    self.host = host or 'localhost'
+    self.port = port or 6379
     self.redis_client = None
   
   def init(self):
-    self.redis_client = redis.Redis(host=self.redis_host, port=self.redis_port)
+    self.redis_client = redis.Redis(host=self.host, port=self.port)
   
   def emit_signal(self, flag_id, app_id, status):
     self.redis_client.ts().add(f'{flag_id}:{status}', '*', 1,
