9798f5cad46787f1b743d53781f25cb20867f8a7
Steven Liou <stevenliou@gmail.com>
2022-07-24 14:00:27 -0400

    Refactors FlagManager new_toggler and set_flags

--- src/tailslide/flag_manager.py
+++ src/tailslide/flag_manager.py
@@ -6,7 +6,7 @@ from toggler import Toggler
 class FlagManager:
   def __init__(self, nats_server='', stream='', app_id='', sdk_key='', user_context='', redis_host='', redis_port=None):
     nats_server = f"nats://{nats_server}"
-    self.nats_client = NatsClient(server=nats_server, stream=stream, subject=app_id, callback=self._set_flags, token=sdk_key)
+    self.nats_client = NatsClient(server=nats_server, stream=stream, subject=app_id, callback=self.set_flags, token=sdk_key)
     self.redis_ts_client = RedisTimeSeriesClient(redis_host, redis_port)
     
     self.flags = []
@@ -17,7 +17,7 @@ class FlagManager:
     self.redis_ts_client.init()
     
   
-  def _set_flags(self, flags):
+  def set_flags(self, flags):
     self.flags= flags
   
   def get_flags(self):
@@ -28,10 +28,9 @@ class FlagManager:
     self.redis_ts_client.disconnect()
 
   def new_toggler(self,config):
-    new_toggle = Toggler(**config, get_flags=self.get_flags, user_context=self.user_context,
+    return Toggler(**config, get_flags=self.get_flags, user_context=self.user_context,
                         emit_redis_signal=self.redis_ts_client.emit_signal
-                         )
-    return new_toggle
+                   )
   
 
 
